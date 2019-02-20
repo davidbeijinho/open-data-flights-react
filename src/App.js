4e550980610ff632ba87824ef742bbbbce952b39
davidbeijinho <davidbeijinho@gmail.com>
2019-02-21 00:05:33 +0100

    add first files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
+import Datamap from 'datamaps';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  // var map = new Datamap({
+  //   element: document.getElementById('container'),
+  //   scope: 'usa'
+  // });
+  drawMap() {
+    console.log(document.getElementById('container'));
+    var map = new Datamap({
+      element: document.getElementById('container'),
+      scope: 'usa'
+    });
+  }
+
+  componentDidMount() {
+    console.log('MOUNTTT');
+    this.drawMap();
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+      <div id="container"></div>
     );
   }
 }
