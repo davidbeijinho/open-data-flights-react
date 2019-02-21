4914cc4e5e9d4ca5e8a9c765979921674c769da9
davidbeijinho <davidbeijinho@gmail.com>
2019-02-21 01:53:02 +0100

    add arcs, and symbolic link

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Datamap from 'datamaps';
 import './App.css';
 
 import { airports, colors } from './lib/airports';
-
+let map;
 class App extends Component {
+ 
   drawMap() {
     console.log(colors);
-    const map = new Datamap({
+    map = new Datamap({
       //   height: '100%', //if not null, datamaps will grab the height of 'element'
       // width: '100%',
       responsive: true,
@@ -20,7 +21,7 @@ class App extends Component {
       fills: {
         defaultFill: '#ABDDA4',
         ...colors
-      },
+      }
       // bubblesConfig: {
       //   borderWidth: 0,
       //   borderOpacity: 0,
@@ -28,6 +29,153 @@ class App extends Component {
       // }
     });
     map.bubbles(airports);
+
+
+
+   
+
+  }
+
+  onClick() {
+    var presidentialTrips = [
+      {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: 32.066667,
+              longitude: 34.783333 
+          },
+          options: {
+            strokeWidth: Math.random() * 12,
+            strokeColor: 'rgba(100, 10, 200, 0.4)',
+            greatArc: true
+          }
+      },
+      {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: 19.433333,
+              longitude: -99.133333
+            },
+            options: {
+              strokeWidth: Math.random() * 12,
+              strokeColor: 'rgba(100, 10, 200, 0.4)',
+              greatArc: true
+            }
+      },
+      {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: 9.933333,
+              longitude: -84.083333
+            },
+            options: {
+              strokeWidth: Math.random() * 12,
+              strokeColor: 'rgba(100, 10, 200, 0.4)',
+              greatArc: true
+            }
+      },
+      {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: 54.597 ,
+              longitude: -5.93
+            },
+            options: {
+              strokeWidth: Math.random() * 12,
+              strokeColor: 'rgba(100, 10, 200, 0.4)',
+              greatArc: true
+            }
+      },
+      {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: 52.516667,
+              longitude: 13.383333 
+            },
+            options: {
+              strokeWidth: Math.random() * 12,
+              strokeColor: 'rgba(100, 10, 200, 0.4)',
+              greatArc: true
+            }
+      },
+      {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: 14.692778,
+              longitude: -17.446667
+            },
+            options: {
+              strokeWidth: Math.random() * 12,
+              strokeColor: 'rgba(100, 10, 200, 0.4)',
+              greatArc: true
+            }
+      },
+      {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: -26.204444,
+              longitude: 28.045556
+            },
+            options: {
+              strokeWidth: Math.random() * 12,
+              strokeColor: 'rgba(100, 10, 200, 0.4)',
+              greatArc: true
+            }
+      },
+              {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: -6.8,
+              longitude: 39.283333 
+          }
+      },
+              {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: 59.329444,
+              longitude: 18.068611
+          }
+      },
+              {
+          origin: {
+              latitude: 38.895111,
+              longitude: -77.036667
+          },
+          destination: {
+              latitude: 59.95 ,
+              longitude: 30.3
+          }
+      }
+  ];
+    console.log('uaaaaaa');
+    map.arc( presidentialTrips, {strokeWidth: 2});
   }
 
   componentDidMount() {
@@ -36,7 +184,12 @@ class App extends Component {
   }
 
   render() {
-    return <div id="container" />;
+    return (
+      <div>
+        <div id="container" />
+        <button onClick={this.onClick}>+</button>
+      </div>
+    );
   }
 }
 
