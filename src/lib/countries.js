72af5b7faee2e8a6d0b0dffd092fa2b8e1dcef9c
davidbeijinho <davidbeijinho@gmail.com>
2019-03-12 00:15:22 +0100

    use only EU contries

--- src/lib/countries.js
+++ src/lib/countries.js
@@ -1,10 +1,10 @@
-import countries from '../data/countries/filtred-countries.json';
+import countries from '../data/countries/active-countries.json';
 
 const countryList = countries.map((value) => {
   return {
     id: value.code,
     label: value.name,
-    value: value.code
+    value: value.downloadCode
   };
 });
 
